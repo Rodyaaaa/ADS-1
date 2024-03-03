a66368092dc98f5607bf5958612839ac197027e2
Rodyaaaa <160117492+Rodyaaaa@users.noreply.github.com>
2024-03-04 01:34:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,25 +23,25 @@ uint64_t nPrime(uint64_t n) {
         }
         if (cnt < n) {
                     number += 2;
-		}
-	}
-	return number;
+        }
+    }
+    return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	uint64_t next = value + 1;
-	while (!checkPrime(next)) {
-		next++;
-	}
-	return next;
+    uint64_t next = value + 1;
+    while (!checkPrime(next)) {
+        next++;
+    }
+    return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sum = 0;
-	for (int i = 2; i < hbound; i++) {
-		if (checkPrime(i)) {
-			sum += i;
-		}
-	}
-	return sum;
+    uint64_t sum = 0;
+    for (int i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
